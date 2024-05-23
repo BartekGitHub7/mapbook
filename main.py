0c2a4280789cac30faccfbeba2676eb4aa31dbf1
BartekGitHub7 <168067440+BartekGitHub7@users.noreply.github.com>
2024-05-23 13:01:21 +0200

    wip: add update function

--- main.py
+++ main.py
@@ -48,5 +48,26 @@ def delete_user(users: list) -> None:
     for user in users:
         if user['name'] == name:
             users.remove(user)
+
+
 # delete_user(data_of_users)
 # read(data_of_users)
+def update_user(users: list) -> None:
+    """
+    update user from a user list
+    :param users: user list
+    :return: None
+    """
+    name: str = input("Enter name of user to be modified: ")
+    for user in users:
+        if user['name'] == name:
+            new_name: str = input("Enter new name: ")
+            new_surname: str = input("Enter new surname: ")
+            new_posts: int = int(input("Enter new number of posts: "))
+            new_location: str = input("Enter new location: ")
+            user['name'] = new_name
+            user['surname'] = new_surname
+            user['posts'] = new_posts
+            user['location'] = new_location
+update_user(data_of_users)
+read(data_of_users)
